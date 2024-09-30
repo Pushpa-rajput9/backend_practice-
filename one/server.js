144b76102bf98ec53cee5530298495324d3ce976
pushpa rajput <pr4541285@gmail.com>
2024-09-30 21:16:19 +0530

    first commit

---
+++ one/server.js
@@ -0,0 +1,12 @@
+const { log } = require("console");
+const http = require("http");
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader(`content-type`, `text/plain`);
+  res.end("node server created by Pushpa Rajput");
+});
+const port = 3000;
+const hostName = "localhost";
+server.listen(port, () => {
+  console.log(`server running at ${hostName}:${port}`);
+});
